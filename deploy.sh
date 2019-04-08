1b89e1700b85e1e48fcca3e66e4583014e5b36a4
de-jcup <albert.tregnaghi@gmail.com>
2019-04-08 13:35:57 +0200

    Deployment 1.4.2 #57

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 BINTRAY_USER=$1
 BINTRAY_API_KEY=$2
+BINTRAY_VERSION=$3
 
 function show_help_and_exit() {
 	echo "Usage:"
-	echo " deploy.sh bintrayUser apiKey"
+	echo " deploy.sh bintrayUser apiKey version"
+	echo " "
+	echo " version can be e.g. 1.4.2"
 	exit 1
 }
 
@@ -16,4 +19,8 @@ if [ -z "$BINTRAY_API_KEY" ]; then
 	echo "bintray api key not set"
 	show_help_and_exit;
 fi
-./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup yamleditor update-site current ./yamleditor-updatesite/
+if [ -z "$BINTRAY_VERSION" ]; then
+    echo "bintray version not set"
+    show_help_and_exit;
+fi
+./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup yamleditor update-site ${BINTRAY_VERSION} ./yamleditor-updatesite/
