b16f38d5bda1ab6a448cc52a33a42046647828a3
de-jcup <albert.tregnaghi@gmail.com>
2019-10-16 13:15:26 +0200

    Added collapse and expand actions,closes #74
    - expanse, collapse all
    - expanse, collapse children

--- yamleditor-plugin/src/main/java-eclipse/de/jcup/yamleditor/outline/YamlEditorContentOutlinePage.java
+++ yamleditor-plugin/src/main/java-eclipse/de/jcup/yamleditor/outline/YamlEditorContentOutlinePage.java
@@ -18,6 +18,7 @@ package de.jcup.yamleditor.outline;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
@@ -25,182 +26,279 @@ import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IWorkbenchCommandConstants;
 import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
 
+import de.jcup.yamleditor.EclipseUtil;
 import de.jcup.yamleditor.YamlEditor;
 import de.jcup.yamleditor.YamlEditorActivator;
-import de.jcup.yamleditor.outline.Item;
-import de.jcup.yamleditor.EclipseUtil;
 import de.jcup.yamleditor.script.YamlScriptModel;
 
 public class YamlEditorContentOutlinePage extends ContentOutlinePage implements IDoubleClickListener {
-	private static ImageDescriptor IMG_DESC_LINKED = EclipseUtil.createImageDescriptor("/icons/outline/synced.png",
-			YamlEditorActivator.PLUGIN_ID);
-	private static ImageDescriptor IMG_DESC_NOT_LINKED = EclipseUtil
-			.createImageDescriptor("/icons/outline/sync_broken.png", YamlEditorActivator.PLUGIN_ID);
+    
+    private static ImageDescriptor IMG_DESC_LINKED = createImageDescriptor("/icons/outline/synced.png");
+    private static ImageDescriptor IMG_DESC_NOT_LINKED =createImageDescriptor("/icons/outline/sync_broken.png");
+    private static ImageDescriptor IMG_DESC_EXPAND_ALL = createImageDescriptor("/icons/expandall.png");
+    private static ImageDescriptor IMG_DESC_COLLAPSE_ALL = createImageDescriptor("/icons/collapseall.png");
 
-	private YamlEditorTreeContentProvider contentProvider;
-	private Object input;
-	private YamlEditor editor;
-	private YamlEditorOutlineLabelProvider labelProvider;
+    private YamlEditorTreeContentProvider contentProvider;
+    private Object input;
+    private YamlEditor editor;
+    private YamlEditorOutlineLabelProvider labelProvider;
 
-	private boolean linkingWithEditorEnabled;
-	private boolean ignoreNextSelectionEvents;
-	private ToggleLinkingAction toggleLinkingAction;
+    private boolean linkingWithEditorEnabled;
+    private boolean ignoreNextSelectionEvents;
+    private ToggleLinkingAction toggleLinkingAction;
+    private ISelection selection;
 
-	public YamlEditorContentOutlinePage(YamlEditor editor) {
-		this.editor = editor;
-		this.contentProvider = new YamlEditorTreeContentProvider();
-	}
+    public YamlEditorContentOutlinePage(YamlEditor editor) {
+        this.editor = editor;
+        this.contentProvider = new YamlEditorTreeContentProvider();
+    }
 
-	public YamlEditorTreeContentProvider getContentProvider() {
-		return contentProvider;
-	}
+    private static ImageDescriptor createImageDescriptor(String name) {
+        return EclipseUtil.createImageDescriptor(name, YamlEditorActivator.PLUGIN_ID);
+    }
 
-	public void createControl(Composite parent) {
-		super.createControl(parent);
+    public YamlEditorTreeContentProvider getContentProvider() {
+        return contentProvider;
+    }
 
-		labelProvider = new YamlEditorOutlineLabelProvider();
+    public void createControl(Composite parent) {
+        super.createControl(parent);
 
-		TreeViewer viewer = getTreeViewer();
-		viewer.setContentProvider(contentProvider);
-		viewer.addDoubleClickListener(this);
-		viewer.setLabelProvider(new DelegatingStyledCellLabelProvider(labelProvider));
-		viewer.addSelectionChangedListener(this);
-		/* next line enables tooltips for tree viewer- necesseary, otherwise not working!*/
-		ColumnViewerToolTipSupport.enableFor(viewer);
+        labelProvider = new YamlEditorOutlineLabelProvider();
 
-		/* it can happen that input is already updated before control created */
-		if (input != null) {
-			viewer.setInput(input);
-		}
-		toggleLinkingAction = new ToggleLinkingAction();
-		toggleLinkingAction.setActionDefinitionId(IWorkbenchCommandConstants.NAVIGATE_TOGGLE_LINK_WITH_EDITOR);
-		
-		IActionBars actionBars = getSite().getActionBars();
-		
-		IToolBarManager toolBarManager = actionBars.getToolBarManager();
-		toolBarManager.add(toggleLinkingAction);
-		
-		IMenuManager viewMenuManager = actionBars.getMenuManager();
-		viewMenuManager.add(new Separator("EndFilterGroup")); //$NON-NLS-1$
-	
-		viewMenuManager.add(new Separator("treeGroup")); //$NON-NLS-1$
-		viewMenuManager.add(toggleLinkingAction);
-		
-		
-		/*
-		 * when no input is set on init state - let the editor rebuild outline
-		 * (async)
-		 */
-		if (input == null && editor != null) {
-			editor.rebuildOutline();
-		}
+        TreeViewer viewer = getTreeViewer();
+        viewer.setContentProvider(contentProvider);
+        viewer.addDoubleClickListener(this);
+        viewer.setLabelProvider(new DelegatingStyledCellLabelProvider(labelProvider));
+        viewer.addSelectionChangedListener(this);
+        
+        ExpandAllAction expandAllAction = new ExpandAllAction();
+        CollapseAllAction collapseAllAction = new CollapseAllAction();
+        ExpandSelectionAction expandSelectionAction = new ExpandSelectionAction();
+        CollapseSelectionAction collapseSelectionAction = new CollapseSelectionAction();
+        
+        MenuManager menuMgr = new MenuManager();
+        menuMgr.add(expandSelectionAction);
+        menuMgr.add(collapseSelectionAction);
+        
+        Menu menu = menuMgr.createContextMenu(viewer.getTree());
+        viewer.getControl().setMenu(menu);
+        
+        /*
+         * next line enables tooltips for tree viewer- necesseary, otherwise not
+         * working!
+         */
+        ColumnViewerToolTipSupport.enableFor(viewer);
 
-	}
+        /* it can happen that input is already updated before control created */
+        if (input != null) {
+            viewer.setInput(input);
+        }
+        toggleLinkingAction = new ToggleLinkingAction();
+        toggleLinkingAction.setActionDefinitionId(IWorkbenchCommandConstants.NAVIGATE_TOGGLE_LINK_WITH_EDITOR);
 
-	@Override
-	public void doubleClick(DoubleClickEvent event) {
-		if (editor == null) {
-			return;
-		}
-		if (linkingWithEditorEnabled) {
-			editor.setFocus();
-			// selection itself is already handled by single click
-			return;
-		}
-		ISelection selection = event.getSelection();
-		editor.openSelectedTreeItemInEditor(selection, true);
-	}
+        IActionBars actionBars = getSite().getActionBars();
 
-	@Override
-	public void selectionChanged(SelectionChangedEvent event) {
-		super.selectionChanged(event);
-		if (!linkingWithEditorEnabled) {
-			return;
-		}
-		if (ignoreNextSelectionEvents) {
-			return;
-		}
-		ISelection selection = event.getSelection();
-		editor.openSelectedTreeItemInEditor(selection, false);
-	}
+        IToolBarManager toolBarManager = actionBars.getToolBarManager();
+        toolBarManager.add(expandAllAction);
+        toolBarManager.add(collapseAllAction);
+        toolBarManager.add(toggleLinkingAction);
 
-	public void onEditorCaretMoved(int caretOffset) {
-		if (!linkingWithEditorEnabled) {
-			return;
-		}
-		ignoreNextSelectionEvents = true;
-		if (contentProvider instanceof YamlEditorTreeContentProvider) {
-			YamlEditorTreeContentProvider gcp = (YamlEditorTreeContentProvider) contentProvider;
-			Item item = gcp.tryToFindByOffset(caretOffset);
-			if (item != null) {
-				StructuredSelection selection = new StructuredSelection(item);
-				getTreeViewer().setSelection(selection, true);
-			}
-		}
-		ignoreNextSelectionEvents = false;
-	}
+        IMenuManager viewMenuManager = actionBars.getMenuManager();
+        viewMenuManager.add(new Separator("EndFilterGroup")); //$NON-NLS-1$
+        
+        viewMenuManager.add(new Separator("treeGroup")); //$NON-NLS-1$
+        viewMenuManager.add(toggleLinkingAction);
+        viewMenuManager.add(expandAllAction);
+        viewMenuManager.add(collapseAllAction);
+        viewMenuManager.add(new Separator("treeSelectionGroup")); //$NON-NLS-1$
+        viewMenuManager.add(expandSelectionAction);
+        viewMenuManager.add(collapseSelectionAction);
 
-	public void rebuild(YamlScriptModel model) {
-		if (model == null) {
-			return;
-		}
-		contentProvider.rebuildTree(model);
+        /*
+         * when no input is set on init state - let the editor rebuild outline (async)
+         */
+        if (input == null && editor != null) {
+            editor.rebuildOutline();
+        }
 
-		TreeViewer treeViewer = getTreeViewer();
-		if (treeViewer != null) {
-			Control control = treeViewer.getControl();
-			if (control == null || control.isDisposed()){
-				return;
-			}
-			treeViewer.setInput(model);
-		}
-	}
+    }
 
-	class ToggleLinkingAction extends Action {
+    @Override
+    public void doubleClick(DoubleClickEvent event) {
+        if (editor == null) {
+            return;
+        }
+        if (linkingWithEditorEnabled) {
+            editor.setFocus();
+            // selection itself is already handled by single click
+            return;
+        }
+        ISelection selection = event.getSelection();
+        editor.openSelectedTreeItemInEditor(selection, true);
+    }
 
-		private ToggleLinkingAction() {
-			if (editor != null) {
-				linkingWithEditorEnabled = editor.getPreferences().isLinkOutlineWithEditorEnabled();
-			}
-			setDescription("link with editor");
-			initImage();
-			initText();
-		}
+    @Override
+    public void selectionChanged(SelectionChangedEvent event) {
+        super.selectionChanged(event);
+        if (!linkingWithEditorEnabled) {
+            return;
+        }
+        if (ignoreNextSelectionEvents) {
+            return;
+        }
+        selection = event.getSelection();
+        editor.openSelectedTreeItemInEditor(selection, false);
+    }
 
-		@Override
-		public void run() {
-			linkingWithEditorEnabled = !linkingWithEditorEnabled;
+    public void onEditorCaretMoved(int caretOffset) {
+        if (!linkingWithEditorEnabled) {
+            return;
+        }
+        ignoreNextSelectionEvents = true;
+        if (contentProvider instanceof YamlEditorTreeContentProvider) {
+            YamlEditorTreeContentProvider gcp = (YamlEditorTreeContentProvider) contentProvider;
+            Item item = gcp.tryToFindByOffset(caretOffset);
+            if (item != null) {
+                StructuredSelection selection = new StructuredSelection(item);
+                getTreeViewer().setSelection(selection, true);
+            }
+        }
+        ignoreNextSelectionEvents = false;
+    }
+    
+    private Object getFirstSelectedElement() {
+        if (! (selection instanceof IStructuredSelection)) {
+            return null;
+        }
+        IStructuredSelection ss = (IStructuredSelection) selection;
+        Object element = ss.getFirstElement();
+        return element;
+    }
 
-			initText();
-			initImage();
-		}
+    public void rebuild(YamlScriptModel model) {
+        if (model == null) {
+            return;
+        }
+        contentProvider.rebuildTree(model);
 
-		private void initImage() {
-			setImageDescriptor(
-					linkingWithEditorEnabled ? getImageDescriptionForLinked() : getImageDescriptionNotLinked());
-		}
+        TreeViewer treeViewer = getTreeViewer();
+        if (treeViewer != null) {
+            Control control = treeViewer.getControl();
+            if (control == null || control.isDisposed()) {
+                return;
+            }
+            treeViewer.setInput(model);
+        }
+    }
 
-		private void initText() {
-			setText(linkingWithEditorEnabled ? "Click to unlink from editor" : "Click to link with editor");
-		}
+    class CollapseAllAction extends Action {
 
-	}
+        private CollapseAllAction() {
+            setImageDescriptor(IMG_DESC_COLLAPSE_ALL);
+            setText("Collapse all");
+        }
 
-	protected ImageDescriptor getImageDescriptionForLinked() {
-		return IMG_DESC_LINKED;
-	}
+        @Override
+        public void run() {
+            getTreeViewer().collapseAll();
+        }
+    }
 
-	protected ImageDescriptor getImageDescriptionNotLinked() {
-		return IMG_DESC_NOT_LINKED;
-	}
+    class ExpandAllAction extends Action {
+
+        private ExpandAllAction() {
+            setImageDescriptor(IMG_DESC_EXPAND_ALL);
+            setText("Expand all");
+        }
+
+        @Override
+        public void run() {
+            getTreeViewer().expandAll();
+        }
+    }
+    
+    class ExpandSelectionAction extends Action {
+
+        private ExpandSelectionAction() {
+            setImageDescriptor(IMG_DESC_EXPAND_ALL);
+            setText("Expand children");
+        }
+
+        @Override
+        public void run() {
+            Object element = getFirstSelectedElement();
+            if (element==null) {
+                return ;
+            }
+            getTreeViewer().expandToLevel(element,TreeViewer.ALL_LEVELS);
+        }
+    }
+
+    class CollapseSelectionAction extends Action {
+
+        private CollapseSelectionAction() {
+            setImageDescriptor(IMG_DESC_COLLAPSE_ALL);
+            setText("Collapse children");
+        }
+
+        @Override
+        public void run() {
+            Object element = getFirstSelectedElement();
+            if (element==null) {
+                return ;
+            }
+            getTreeViewer().collapseToLevel(element,TreeViewer.ALL_LEVELS);
+        }
+       
+    }
+    
+    class ToggleLinkingAction extends Action {
+
+        private ToggleLinkingAction() {
+            if (editor != null) {
+                linkingWithEditorEnabled = editor.getPreferences().isLinkOutlineWithEditorEnabled();
+            }
+            setDescription("link with editor");
+            initImage();
+            initText();
+        }
+
+        @Override
+        public void run() {
+            linkingWithEditorEnabled = !linkingWithEditorEnabled;
+
+            initText();
+            initImage();
+        }
+
+        private void initImage() {
+            setImageDescriptor(linkingWithEditorEnabled ? getImageDescriptionForLinked() : getImageDescriptionNotLinked());
+        }
+
+        private void initText() {
+            setText(linkingWithEditorEnabled ? "Click to unlink from editor" : "Click to link with editor");
+        }
+
+    }
+
+    protected ImageDescriptor getImageDescriptionForLinked() {
+        return IMG_DESC_LINKED;
+    }
+
+    protected ImageDescriptor getImageDescriptionNotLinked() {
+        return IMG_DESC_NOT_LINKED;
+    }
 
 }
